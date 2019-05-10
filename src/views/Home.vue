99901426b25231e3be97efa96021cd842d2b6fff
e <changzzc@126.com>
2019-05-10 16:21:38 +0800

    菜单样式调整

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,9 +9,9 @@
 					<i class="fa fa-align-justify"></i>
 				</div>
 			</el-col>
-			<el-col :span="4" class="userinfo">
+			<el-col :span="4" class="userInfo">
 				<el-dropdown trigger="hover">
-					<span class="el-dropdown-link userinfo-inner"><img :src="this.sysUserAvatar" /> {{sysUserName}}</span>
+					<span class="el-dropdown-link userInfo-inner"><img :src="this.sysUserAvatar" /> {{sysUserName}}</span>
 					<el-dropdown-menu slot="dropdown">
 						<el-dropdown-item>我的消息</el-dropdown-item>
 						<el-dropdown-item>设置</el-dropdown-item>
@@ -21,35 +21,22 @@
 			</el-col>
 		</el-col>
 		<el-col :span="24" class="main">
-			<aside :class="collapsed?'menu-collapsed':'menu-expanded'">
-				<!--导航菜单-->
-				<el-menu :default-active="$route.path" class="el-menu-vertical-demo" @open="handleopen" @close="handleclose" @select="handleselect"
-					 unique-opened router v-show="!collapsed">
-					<template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
-						<el-submenu :index="index+''" v-if="!item.leaf">
-							<template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
-							<el-menu-item v-for="child in item.children" :index="child.path" :key="child.path" v-if="!child.hidden">{{child.name}}</el-menu-item>
-						</el-submenu>
-						<el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path"><i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>
-					</template>
-				</el-menu>
-				<!--导航菜单-折叠后-->
-				<ul class="el-menu el-menu-vertical-demo collapsed" v-show="collapsed" ref="menuCollapsed">
-					<li v-for="(item,index) in $router.options.routes" v-if="!item.hidden" class="el-submenu item">
-						<template v-if="!item.leaf">
-							<div class="el-submenu__title" style="padding-left: 20px;" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)"><i :class="item.iconCls"></i></div>
-							<ul class="el-menu submenu" :class="'submenu-hook-'+index" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)"> 
-								<li v-for="child in item.children" v-if="!child.hidden" :key="child.path" class="el-menu-item" style="padding-left: 40px;" :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">{{child.name}}</li>
-							</ul>
+			<el-menu :default-active="$route.path" class="el-menu-vertical-demo" @open="handleopen" @close="handleclose" @select="handleselect"
+					 unique-opened router :collapse="collapsed">
+				<template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
+					<el-submenu :index="index+''" v-if="!item.leaf">
+						<template slot="title">
+							<i :class="item.iconCls"></i>
+							<span>{{item.name}}</span>
 						</template>
-						<template v-else>
-							<li class="el-submenu">
-								<div class="el-submenu__title el-menu-item" style="padding-left: 20px;height: 56px;line-height: 56px;padding: 0 20px;" :class="$route.path==item.children[0].path?'is-active':''" @click="$router.push(item.children[0].path)"><i :class="item.iconCls"></i></div>
-							</li>
-						</template>
-					</li>
-				</ul>
-			</aside>
+						<el-menu-item v-for="child in item.children" :index="child.path" :key="child.path" v-if="!child.hidden">{{child.name}}</el-menu-item>
+					</el-submenu>
+					<el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path">
+						<i :class="item.iconCls"></i>
+						<span slot="title">{{item.children[0].name}}</span>
+					</el-menu-item>
+				</template>
+			</el-menu>
 			<section class="content-container">
 				<div class="grid-content bg-purple-light">
 					<el-col :span="24" class="breadcrumb-container">
@@ -79,22 +66,10 @@
 				collapsed:false,
 				sysUserName: '',
 				sysUserAvatar: '',
-				form: {
-					name: '',
-					region: '',
-					date1: '',
-					date2: '',
-					delivery: false,
-					type: [],
-					resource: '',
-					desc: ''
-				}
+
 			}
 		},
 		methods: {
-			onSubmit() {
-				console.log('submit!');
-			},
 			handleopen() {
 				//console.log('handleopen');
 			},
@@ -118,14 +93,10 @@
 			//折叠导航栏
 			collapse:function(){
 				this.collapsed=!this.collapsed;
-			},
-			showMenu(i,status){
-				this.$refs.menuCollapsed.getElementsByClassName('submenu-hook-'+i)[0].style.display=status?'block':'none';
 			}
 		},
 		created(){
 		  var user = "{\"id\":1,\"username\":\"admin\",\"avatar\":\"https://raw.githubusercontent.com/taylorchen709/markdown-images/master/vueadmin/user.png\",\"name\":\"张某某\"}";
-
 		  sessionStorage.setItem("user" , user);
 		},
 		mounted() {
@@ -143,7 +114,8 @@
 
 <style scoped lang="scss">
 	@import '~scss_vars';
-	
+
+	.el-menu-vertical-demo:not(.el-menu--collapse) {width: 230px;min-height: 600px;}
 	.container {
 		position: absolute;
 		top: 0px;
@@ -154,24 +126,17 @@
 			line-height: 60px;
 			background: $color-primary;
 			color:#fff;
-			.userinfo {
+			.userInfo {
 				text-align: right;
 				padding-right: 35px;
 				float: right;
-				.userinfo-inner {
+				.userInfo-inner {
 					cursor: pointer;
 					color:#fff;
-					img {
-						width: 40px;
-						height: 40px;
-						border-radius: 20px;
-						margin: 10px 0px 10px 10px;
-						float: right;
-					}
+					img {width: 40px;height: 40px;border-radius: 20px;margin: 10px 0px 10px 10px;float: right;}
 				}
 			}
 			.logo {
-				//width:230px;
 				height:60px;
 				font-size: 22px;
 				padding-left:20px;
@@ -179,94 +144,30 @@
 				border-color: rgba(238,241,146,0.3);
 				border-right-width: 1px;
 				border-right-style: solid;
-				img {
-					width: 40px;
-					float: left;
-					margin: 10px 10px 10px 18px;
-				}
-				.txt {
-					color:#fff;
-				}
+				img {width: 40px;float: left;margin: 10px 10px 10px 18px;}
+				.txt {color:#fff;}
 			}
-			.logo-width{
-				width:230px;
-			}
-			.logo-collapse-width{
-				width:60px
-			}
-			.tools{
-				padding: 0px 23px;
-				width:14px;
-				height: 60px;
-				line-height: 60px;
-				cursor: pointer;
-			}
+			.logo-width{width:230px;}
+			.logo-collapse-width{width:60px}
+			.tools{padding: 0px 23px;width:14px;height: 60px;line-height: 60px;cursor: pointer;}
 		}
 		.main {
 			display: flex;
-			// background: #324057;
 			position: absolute;
 			top: 60px;
 			bottom: 0px;
 			overflow: hidden;
-			aside {
-				flex:0 0 230px;
-				width: 230px;
-				// position: absolute;
-				// top: 0px;
-				// bottom: 0px;
-				.el-menu{
-					height: 100%;
-				}
-				.collapsed{
-					width:60px;
-					.item{
-						position: relative;
-					}
-					.submenu{
-						position:absolute;
-						top:0px;
-						left:60px;
-						z-index:99999;
-						height:auto;
-						display:none;
-					}
 
-				}
-			}
-			.menu-collapsed{
-				flex:0 0 60px;
-				width: 60px;
-			}
-			.menu-expanded{
-				flex:0 0 230px;
-				width: 230px;
-			}
+			.el-menu--collapse{width: 60px;min-height: 600px;}
 			.content-container {
-				// background: #f1f2f7;
 				flex:1;
-				// position: absolute;
-				// right: 0px;
-				// top: 0px;
-				// bottom: 0px;
-				// left: 230px;
 				overflow-y: scroll;
 				padding: 20px;
 				.breadcrumb-container {
-					//margin-bottom: 15px;
-					.title {
-						width: 200px;
-						float: left;
-						color: #475669;
-					}
-					.breadcrumb-inner {
-						float: right;
-					}
+					.title {width: 200px;float: left;color: #475669;}
+					.breadcrumb-inner {float: right;}
 				}
-				.content-wrapper {
-					background-color: #fff;
-					box-sizing: border-box;
-				}
+				.content-wrapper {background-color: #fff;box-sizing: border-box;}
 			}
 		}
 	}
